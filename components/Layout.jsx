7a10d3d2c7530c27132689c0a09bee64bc0b7f2e
aditya850 <adityatripathi255@gmail.com>
2022-11-11 13:40:20 +0530

    p

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>JS Mastery Store</title>
+        <title>BUZZ BUYS</title>
       </Head>
       <header>
         <Navbar />
